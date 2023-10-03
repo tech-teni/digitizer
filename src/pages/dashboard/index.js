cbc57a6321e2756b2acacc841cacb5d86cbd9f5d
tech-teni <yakubganiyat96@gmail.com>
2023-10-03 21:49:39 +0100

    first commit

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -5,30 +5,37 @@ const Dashboard = () => {
   return (
     <>
       <Header />
-      <section className="spatial-section">
-        <div className="main-nav">
-          <h5>Spartial Templates</h5>
-        </div>
+      <div className="content-container">
+        <section className="spatial-section">
+          <div className="main-nav">
+            <h5>Spartial Templates</h5>
+          </div>
 
-        <div className="search-container">
-          <input type="search" />
-          <img src="" alt="" />
-        </div>
-        <p>recently created</p>
-
-        <ul>
-          <li>
-            <img src="" alt="" />
+          <div className="search-container">
+            <input type="search" placeholder="Search" />
             <div>
-              <h6>Ambulance</h6>
-              <div>
-                <span>Bus</span>
-                <span>Medical</span>
-              </div>
+              <img src="" alt="" />
             </div>
-          </li>
-        </ul>
-      </section>
+          </div>
+          <p>recently created</p>
+
+          <ul className="option-list">
+            <li>
+              <div className="image-container">
+                {/* <img src="" alt="" /> */}
+              </div>
+              <div className="list-info">
+                <h6>Ambulance</h6>
+                <div>
+                  <span>Bus</span>
+                  <span>Medical</span>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </section>
+        <section></section>
+      </div>
     </>
   );
 };
